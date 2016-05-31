52f8640b0e58076603e14d41f1d1080e3ebc76d7
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-05-31 15:45:43 +0530

    ctrl

--- src/CrudController.php
+++ src/CrudController.php
@@ -1,1 +1,18 @@
-use Illuminate\Support\Facades\Blade;
+<?php namespace Dwijitso\Sbscrud;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Config;
+
+class CrudController extends Controller
+{
+	/**
+	 * Show the application welcome screen to the user.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		// dd(Config::get("contact.message"));
+		// return view('contact::contact');
+	}
+}
