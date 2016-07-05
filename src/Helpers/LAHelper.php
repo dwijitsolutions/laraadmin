1abb7c8eb9cdb7f74cfcc9d177930a98385353ea
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-07-05 19:14:07 +0530

    LAHelper::createThumbnail added

--- src/Helpers/LAHelper.php
+++ src/Helpers/LAHelper.php
@@ -162,4 +162,46 @@ class LAHelper
 		}
 		return $password;
 	}
+	
+	// LAHelper::createThumbnail($filepath, $thumbpath, $thumbnail_width, $thumbnail_height);
+	function createThumbnail($filepath, $thumbpath, $thumbnail_width, $thumbnail_height, $background=false) {
+	    list($original_width, $original_height, $original_type) = getimagesize($filepath);
+	    if ($original_width > $original_height) {
+	        $new_width = $thumbnail_width;
+	        $new_height = intval($original_height * $new_width / $original_width);
+	    } else {
+	        $new_height = $thumbnail_height;
+	        $new_width = intval($original_width * $new_height / $original_height);
+	    }
+	    $dest_x = intval(($thumbnail_width - $new_width) / 2);
+	    $dest_y = intval(($thumbnail_height - $new_height) / 2);
+	    if ($original_type === 1) {
+	        $imgt = "ImageGIF";
+	        $imgcreatefrom = "ImageCreateFromGIF";
+	    } else if ($original_type === 2) {
+	        $imgt = "ImageJPEG";
+	        $imgcreatefrom = "ImageCreateFromJPEG";
+	    } else if ($original_type === 3) {
+	        $imgt = "ImagePNG";
+	        $imgcreatefrom = "ImageCreateFromPNG";
+	    } else {
+	        return false;
+	    }
+	    $old_image = $imgcreatefrom($filepath);
+	    $new_image = imagecreatetruecolor($thumbnail_width, $thumbnail_height); // creates new image, but with a black background
+	    // figuring out the color for the background
+	    if(is_array($background) && count($background) === 3) {
+	      list($red, $green, $blue) = $background;
+	      $color = imagecolorallocate($new_image, $red, $green, $blue);
+	      imagefill($new_image, 0, 0, $color);
+	    // apply transparent background only if is a png image
+	    } else if($background === 'transparent' && $original_type === 3) {
+	      imagesavealpha($new_image, TRUE);
+	      $color = imagecolorallocatealpha($new_image, 0, 0, 0, 127);
+	      imagefill($new_image, 0, 0, $color);
+	    }
+	    imagecopyresampled($new_image, $old_image, $dest_x, $dest_y, 0, 0, $new_width, $new_height, $original_width, $original_height);
+	    $imgt($new_image, $thumbpath);
+	    return file_exists($thumbpath);
+	}
 }
