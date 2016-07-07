8abfb7bbb4bc7eea76fe45ee6a6203d32a447813
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-07-07 14:19:13 +0530

    Menu Migration

---
+++ src/Installs/migrations/2016_07_07_134058_create_menus_table.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Migration genrated using LaraAdmin
+ * Help: http://laraadmin.com
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('la_menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('url', 256);
+            $table->string('icon', 50)->default("fa-cube");
+            $table->string('type', 20)->default("module");
+            $table->integer('parent')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('la_menus')) {
+            Schema::drop('la_menus');
+        }
+    }
+}
