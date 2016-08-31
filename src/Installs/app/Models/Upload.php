031443d94bd5deacc117d5c70c943fde874bc75b
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-08-31 19:47:37 +0530

    Get Upload File Path

--- src/Installs/app/Models/Upload.php
+++ src/Installs/app/Models/Upload.php
@@ -30,4 +30,12 @@ class Upload extends Model
     {
         return $this->belongsTo('App\User');
     }
+    
+    /**
+     * Get File path
+     */
+    public function path()
+    {
+        return url("files/".$this->hash."/".$this->name);
+    }
 }
