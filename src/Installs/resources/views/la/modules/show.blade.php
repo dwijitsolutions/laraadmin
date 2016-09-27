41beb2a16d92b5d6dae152e7a70bcad0fd41a1bb
slozano95 <slozano95@users.noreply.github.com>
2016-09-27 09:48:17 -0500

    Update show.blade.php

--- src/Installs/resources/views/la/modules/show.blade.php
+++ src/Installs/resources/views/la/modules/show.blade.php
@@ -331,7 +331,7 @@ $(function(){
 	
 	var url = window.location.href;
 	var activeTab = url.substring(url.indexOf("#") + 1);
-	if(!activeTab.includes("http")){
+	if(!activeTab.includes("http") && activeTab.length>1){
 		$(".tab-pane").removeClass("active");
  		$('#tab_'+activeTab).click();
  	}
