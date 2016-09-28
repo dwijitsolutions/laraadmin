0c3965ddceea805f968ef484fef0a428a5cf95be
Madhavi Khatal <madhavikhatal@gmail.com>
2016-09-28 21:57:32 +0530

    seeding bug-fix

--- src/Installs/seeds/DatabaseSeeder.php
+++ src/Installs/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(database\seeds\LaraAdminSeeder::class);
+        $this->call(LaraAdminSeeder::class);
     }
 }
