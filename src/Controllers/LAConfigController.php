4278ad71a45ecadb0e1c4478739a813a2d148c3f
slozano95 <slozano95@users.noreply.github.com>
2016-10-06 11:04:25 -0500

    Create LAConfigController.php

---
+++ src/Controllers/LAConfigController.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Controller genrated using LaraAdmin
+ * Help: http://laraadmin.com
+ */
+
+namespace Dwij\Laraadmin\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use DB;
+use Dwij\Laraadmin\Helpers\LAHelper;
+use Dwij\Laraadmin\Models\LAConfigs;
+use App\Role;
+
+class LAConfigController extends Controller
+{
+	
+	public function __construct() {
+		// for authentication (optional)
+		$this->middleware('auth');
+	}
+	
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$configs = LAConfigs::all();
+		$skin_array=['White Skin'=>'skin-white','Blue Skin'=>'skin-blue','Black Skin'=>'skin-black','Purple Skin'=>'skin-purple','Yellow Sking'=>'skin-yellow','Red Skin'=>'skin-red','Green Skin'=>'skin-green'];
+		$layout_array=['Fixed Layout'=>'fixed','Boxed Layout'=>'layout-boxed','Top Navigation Layout'=>'layout-top-nav','Sidebar Collapse Layout'=>'sidebar-collapse','Mini Sidebar Layout'=>'sidebar-mini'];
+		return View('la.la_configs.index', [
+			'configs' => $configs,
+			'skins' => $skin_array,
+			'layouts' => $layout_array
+		]);
+	}
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request){
+		
+		$all = $request->all();
+		foreach($all as $key=>$value){
+			LAConfigs::where('key',$key)->update(['value'=>$value]);
+		}
+		
+		$configs = LAConfigs::all();
+		$skin_array=['White Skin'=>'skin-white','Blue Skin'=>'skin-blue','Black Skin'=>'skin-black','Purple Skin'=>'skin-purple','Yellow Sking'=>'skin-yellow','Red Skin'=>'skin-red','Green Skin'=>'skin-green'];
+		$layout_array=['Fixed Layout'=>'fixed','Boxed Layout'=>'layout-boxed','Top Navigation Layout'=>'layout-top-nav','Sidebar Collapse Layout'=>'sidebar-collapse','Mini Sidebar Layout'=>'sidebar-mini'];
+		return View('la.la_configs.index', [
+			'configs' => $configs,
+			'skins' => $skin_array,
+			'layouts' => $layout_array
+		]);
+	}
+	
+	
+}
