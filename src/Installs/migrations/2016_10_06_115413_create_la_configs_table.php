0f65c693bdf1a75356709339982df9d1843a5dc7
Santiago Lozano <slozano95@gmail.com>
2016-10-06 11:56:18 -0500

    la_configs migration

---
+++ src/Installs/migrations/2016_10_06_115413_create_la_configs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLAConfigTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('la_configs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('key', 50);
+            $table->string('value', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('la_configs');
+    }
+}
