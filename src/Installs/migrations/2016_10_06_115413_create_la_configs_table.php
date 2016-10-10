07c6f195402da141b7f875eb55d12934d41101a9
Sagar Takle <sagarkoshti1990@gmail.com>
2016-10-10 18:41:59 +0530

    CreateLaConfigsTable Class name corrected

--- src/Installs/migrations/2016_10_06_115413_create_la_configs_table.php
+++ src/Installs/migrations/2016_10_06_115413_create_la_configs_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLAConfigsTable extends Migration
+class CreateLaConfigsTable extends Migration
 {
     /**
      * Run the migrations.
