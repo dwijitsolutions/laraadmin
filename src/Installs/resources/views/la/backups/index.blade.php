358e0235af35c18c1d90d6f9f8e8b9cab63dd292
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-10-13 21:32:12 +0530

    Backup manager completed

--- src/Installs/resources/views/la/backups/index.blade.php
+++ src/Installs/resources/views/la/backups/index.blade.php
@@ -45,36 +45,6 @@
 	</div>
 </div>
 
-@la_access("Backups", "create")
-<div class="modal fade" id="AddModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title" id="myModalLabel">Add Backup</h4>
-			</div>
-			{!! Form::open(['action' => 'LA\BackupsController@store', 'id' => 'backup-add-form']) !!}
-			<div class="modal-body">
-				<div class="box-body">
-                    @la_form($module)
-					
-					{{--
-					@la_input($module, 'name')
-					@la_input($module, 'date')
-					@la_input($module, 'file_name')
-					--}}
-				</div>
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				{!! Form::submit( 'Submit', ['class'=>'btn btn-success']) !!}
-			</div>
-			{!! Form::close() !!}
-		</div>
-	</div>
-</div>
-@endla_access
-
 @endsection
 
 @push('styles')
