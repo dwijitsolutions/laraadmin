0d2d7a85b439d7bf1eb8726c5c6833561a170c14
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-10-16 00:30:08 +0530

    Top Navbar Done

--- src/Installs/resources/views/la/layouts/partials/mainheader.blade.php
+++ src/Installs/resources/views/la/layouts/partials/mainheader.blade.php
@@ -22,30 +22,7 @@
 					<i class="fa fa-bars"></i>
 				</button>
 			</div>
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse pull-left" id="navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-					<li><a href="#">Link</a></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="#">Action</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li class="divider"></li>
-							<li><a href="#">Separated link</a></li>
-							<li class="divider"></li>
-							<li><a href="#">One more separated link</a></li>
-						</ul>
-					</li>
-				</ul>
-				<form class="navbar-form navbar-left" role="search">
-					<div class="form-group">
-						<input type="text" class="form-control" id="navbar-search-input" placeholder="Search">
-					</div>
-				</form>
-			</div><!-- /.navbar-collapse -->
+			@include('la.layouts.partials.top_nav_menu')
 			@include('la.layouts.partials.notifs')
 		</div><!-- /.container-fluid -->
 	@else
