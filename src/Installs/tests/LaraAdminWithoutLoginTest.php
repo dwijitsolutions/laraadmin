c0e272a2d0b782e6d4434af3d2de7275a055479a
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-10-18 17:21:18 +0530

    LaraAdminTest Without Login - Half Done

---
+++ src/Installs/tests/LaraAdminWithoutLoginTest.php
@@ -0,0 +1,87 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class LaraAdminWithoutLoginTest extends TestCase
+{
+	use DatabaseMigrations;
+
+	/**
+     * Basic setup before testing
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+		parent::setUp();
+		// Generate Seeds
+		$this->artisan('db:seed');
+    }
+
+	/**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+		$this->visit('/')
+             ->see('LaraAdmin');
+    }
+
+	/**
+     * Test Login Page.
+     *
+     * @return void
+     */
+    public function testLoginPage()
+    {
+		$this->visit('/login')
+            ->seePageIs('/register');
+    }
+
+	/**
+     * Test Register Page.
+     *
+     * @return void
+     */
+    public function testRegisterPage()
+    {
+        $this->visit('/register')
+            ->see('Register Super Admin');
+    }
+
+	/**
+     * Test Registration
+     *
+     * @return void
+     */
+    public function testRegistration()
+    {
+        $this->visit('/register')
+            ->see('Register Super Admin')
+			->type('Taylor Otwell', 'name')
+			->type('test@example.com', 'email')
+			->type('12345678', 'password')
+			->type('12345678', 'password_confirmation')
+			->press('Register')
+			->seePageIs('/')
+			->click('Taylor Otwell')
+			->seePageIs('/admin')
+			->see('Dashboard')
+			->visit('/logout')
+			->seePageIs('/')
+			->click('Login')
+			->type('test@example.com', 'email')
+			->type('12345678', 'password')
+			->press('Sign In')
+			->seePageIs('/')
+			->click('Taylor Otwell')
+			->seePageIs('/admin')
+			->see('Dashboard');
+    }
+
+
+}
