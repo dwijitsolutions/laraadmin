21257a29e1bf4e48dff43e30136100ac41c51c18
Sagar Takle <sagarkoshti1990@gmail.com>
2016-10-21 18:40:58 +0530

    Schema in Migrations

--- src/Installs/migrations/2016_05_26_064006_create_departments_table.php
+++ src/Installs/migrations/2016_05_26_064006_create_departments_table.php
@@ -4,6 +4,7 @@
  * Help: http://laraadmin.com
  */
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Dwij\Laraadmin\Models\Module;
