2397865923379873bf5e06428c036242e4317ac5
imanghafoori <imanghafoori1@gmail.com>
2017-09-07 21:25:11 +0430

    negating conditions and exchanging code blocks same logic as before

--- src/Models/Module.php
+++ src/Models/Module.php
@@ -1207,25 +1207,25 @@ class Module extends Model
     public static function itemCount($module_name)
     {
         $module = Module::get($module_name);
-        if(isset($module)) {
+        if(!isset($module)) {
+            return -1;
+        } else {
             $model_name = ucfirst(str_singular($module_name));
             if($model_name == "User" || $model_name == "Role" || $model_name == "Permission") {
-                if(file_exists(base_path('app/' . $model_name . ".php"))) {
+                if(!file_exists(base_path('app/' . $model_name . ".php"))) {
+                    return -1;
+                } else {
                     $model = "App\\" . $model_name;
                     return $model::count();
-                } else {
-                    return -1;
                 }
             } else {
-                if(file_exists(base_path('app/Models/' . $model_name . ".php"))) {
+                if(!file_exists(base_path('app/Models/' . $model_name . ".php"))) {
+                    return -1;
+                } else {
                     $model = "App\\Models\\" . $model_name;
                     return $model::count();
-                } else {
-                    return -1;
                 }
             }
-        } else {
-            return -1;
         }
     }
     
